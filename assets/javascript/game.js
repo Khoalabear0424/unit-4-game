8060a4277ab94fb1a8bb9f92206f8d0be4b43263
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-01-13 19:01:08 -0800

    added figure with caption

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,10 +30,24 @@ var characters = [
 ];
 
 //---------Load photos of characters to Staging----------//
-
 for(var i=0; i<characters.length; i++){
+var $fig = $('<figure></figure>')
 var $im = $('<img>');
-$im.attr('src',characters[i].src);
+var $cap = $('<figcaption></figcaption>')
 
-$('.stage').eq(i).html($im);
+$im.attr('src',characters[i].src);
+$im.addClass('figure-img img-fluid rounded');
+$cap.text(characters[i].name);
+$fig.addClass('figure-caption text-center');
+
+$fig.append($im);
+$fig.append($cap);
+$('.stage').eq(i).html($fig);
 };
+
+
+//------------Onclick To Staged Images------------//
+$('.stage').on('click',function(){
+    alert($(this).text());
+})
+
