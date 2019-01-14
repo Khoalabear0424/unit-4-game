dfa2ad08375b5a2e0070ffb8b61748c55f45c018
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-01-13 20:48:12 -0800

    select attacker and first defender

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,52 +29,62 @@ var characters = [
     }
 ];
 
+var counter = 0;
+
 //---------Load photos of characters to Staging----------//
-for(var i=0; i<characters.length; i++){
-var $fig = $('<figure></figure>')
-var $im = $('<img>');
-var $cap = $('<figcaption></figcaption>')
+for (var i = 0; i < characters.length; i++) {
+    var $fig = $('<figure></figure>')
+    var $im = $('<img>');
+    var $cap = $('<figcaption></figcaption>')
 
-$im.attr('src',characters[i].src);
-$im.attr('value',i);
-$im.addClass('figure-img img-fluid rounded');
-$cap.text(characters[i].name);
-$fig.addClass('figure-caption text-center');
+    $im.attr('src', characters[i].src);
+    $im.attr('value', i);
+    $im.addClass('figure-img img-fluid rounded');
+    $cap.text(characters[i].name);
+    $fig.addClass('figure-caption text-center');
 
-$fig.append($im);
-$fig.append($cap);
-$('.stage').eq(i).html($fig);
+    $fig.append($im);
+    $fig.append($cap);
+    $('.stage').eq(i).html($fig);
 };
 
 //---------Hide Battle Stage onLoad()----------//
-$('document').ready(function() {
-    window.onload = function(){
+$('document').ready(function () {
+    window.onload = function () {
         $('#battle').hide();
     }
 });
 
 //------------Transition to Battle------------//
-$('img').on('click',function(){
-    var imgPosition = $(this).attr('value');
+$('img').on('click', function () {
+    if (counter == 0) {
+        imgPosition1 = $(this).attr('value');
+        $('#instructions').text('Select the first Defender');
+        counter++;
+        
+    } else if (counter == 1){
+        $('#staging').fadeOut('slow', function () {
+            $('#battle').fadeIn(1000);
+        });
 
-    $('#staging').fadeOut('slow',function(){
-    $('#battle').fadeIn(1000);
-    });
+        //--------------Populate Battle Field----------//
+        imgPosition2 = $(this).attr('value');
+        var attack = $('#attacker');
+        var defender = $('#defender');
+        var allFigure = $('figure');
+        var userChar = $('figure').eq(imgPosition1);
+        var firstDefender = $('figure').eq(imgPosition2);
 
-    //--------------Populate Battle Field----------//
-    var attack = $('#attacker');
-    var defender = $('#defender');
-    var allFigure = $('figure');
-    var userChar = $('figure').eq(imgPosition);
-
-    attack.append(userChar);
-
-    for(var i=0; i<allFigure.length; i++){
-        if(i != imgPosition){
-        defender.append(allFigure.eq(i));
+        attack.append(userChar);
+        defender.append(firstDefender);
+        
+        for (var i = 0; i < allFigure.length; i++) {
+            if (i != imgPosition1 && i!= imgPosition2) {
+                defender.append(allFigure.eq(i));
+            };
         };
-    };
+        counter++
+    }
+
 });
-
-
 
