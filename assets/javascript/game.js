2246d3f77204751b7be02e6cdba922b1fe86b044
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-01-13 20:10:43 -0800

    Transition to battle with attacker and defender

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,28 +4,28 @@ var characters = [
         src: "./assets/images/rey.png",
         health: 10,
         attack: 10,
-        counter: 10,
+        counter: 10
     },
     {
         name: "Han Solo",
         src: "./assets/images/han.png",
         health: 10,
         attack: 10,
-        counter: 10,
+        counter: 10
     },
     {
         name: "Darth Vader",
         src: "./assets/images/vader.png",
         health: 10,
         attack: 10,
-        counter: 10,
+        counter: 10
     },
     {
         name: "Kylo Ren",
         src: "./assets/images/kylo.png",
         health: 10,
         attack: 10,
-        counter: 10,
+        counter: 10
     }
 ];
 
@@ -36,6 +36,7 @@ var $im = $('<img>');
 var $cap = $('<figcaption></figcaption>')
 
 $im.attr('src',characters[i].src);
+$im.attr('value',i);
 $im.addClass('figure-img img-fluid rounded');
 $cap.text(characters[i].name);
 $fig.addClass('figure-caption text-center');
@@ -52,13 +53,28 @@ $('document').ready(function() {
     }
 });
 
-//------------Onclick To Staged Images------------//
-$('figure').on('click',function(){
-    var imgText = $(this).text();
+//------------Transition to Battle------------//
+$('img').on('click',function(){
+    var imgPosition = $(this).attr('value');
 
     $('#staging').fadeOut('slow',function(){
-    //    alert(imgText); 
     $('#battle').fadeIn(1000);
     });
+
+    //--------------Populate Battle Field----------//
+    var attack = $('#attacker');
+    var defender = $('#defender');
+    var allFigure = $('figure');
+    var userChar = $('figure').eq(imgPosition);
+
+    attack.append(userChar);
+
+    for(var i=0; i<allFigure.length; i++){
+        if(i != imgPosition){
+        defender.append(allFigure.eq(i));
+        };
+    };
 });
+
+
 
